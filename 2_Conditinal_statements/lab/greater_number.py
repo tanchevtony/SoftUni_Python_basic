d3b784a9d3845f8e017a480d261372d4aeb6412e
Tony <tony.tanchev@gmail.com>
2020-12-21 01:17:59 +0000

    conditional statement - lab

---
+++ 2_Conditinal_statements/lab/greater_number.py
@@ -0,0 +1,13 @@
+"""
+Намиране на по-голямото число
+Да се напише програма, която чете две цели числа въведени от 
+потребителя и отпечатва по-голямото от двете. 
+
+"""
+number_one = int(input())
+number_two = int(input())
+
+if number_one > number_two:
+    print(number_one)
+else:
+    print(number_two)
