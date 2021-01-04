f8b32df20624fbb0820543883fdbcd3594da255e
Tony Tanchev <tony.tanchev@gmail.com>
2021-01-04 13:24:58 +0000

    max number while loop

---
+++ 5_while_loop/lab/max_number.py
@@ -0,0 +1,20 @@
+"""
+Най-голямо число
+Напишете програма, която до получаване на командата 
+"Stop", чете цели числа, въведени от потребителя,  
+намира най-голямото измежду тях и го принтира. 
+Въвежда се по едно число на ред. 
+"""
+import sys
+
+command = input()
+
+max_number = -sys.maxsize
+
+while command != "Stop":
+    number = int(command)
+    if number > max_number:
+        max_number = number
+    command = input()
+print(max_number)
+    
