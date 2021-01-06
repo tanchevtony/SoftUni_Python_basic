bfaf61c0b8172da1bf4e8714cb014ac75afbcd99
Tony Tanchev <tony.tanchev@gmail.com>
2021-01-06 10:25:34 +0000

    moving update

--- 5_while_loop/lab/moving.py
+++ 5_while_loop/lab/moving.py
@@ -53,4 +53,4 @@ while command != "Done":
 if free_space:
     print(f"{total_volume} Cubic meters left.")
 else:
-    print(f"No more free space! You need {abs(total_volume)} cubic meters more.")
+    print(f"No more free space! You need {abs(total_volume)} Cubic meters more.")
